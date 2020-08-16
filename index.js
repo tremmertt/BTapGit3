45e0a43e16772c9a2dcf411c2f3e96b1a55c0ac1
Tram <tremmertt98@gmail.com>
2020-08-16 23:46:33 +0700

    create_buoi2

--- index.js
+++
@@ -1,9 +0,0 @@
-var lamBaiTap = function() {
-    console.log(lamBaiTap);
-}
-var doneBaiTapBuoi1 = function() {
-    console.log(doneBaiTapBuoi1);
-}
-var doneBaiTapBuoi2 = function() {
-    console.log(doneBaiTapBuoi2);
-}
